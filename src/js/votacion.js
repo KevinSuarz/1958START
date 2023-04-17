f6c55fc598f379d439585e741e52b43bcc8b0148
kevin <kevinsuaga@gmail.com>
2023-04-17 09:58:55 +0200

    ya cambia el color de fondo cuando se clickea en los votos, falta que quite el color de fondo, revisar chat gpt

--- src/js/votacion.js
+++ src/js/votacion.js
@@ -2,5 +2,12 @@
 
 
 function seleccionado (selected){
+  let seleccion = document.querySelector(`#${selected}`);
+  let check = document.querySelector(`.${selected}`);
   console.log(selected);
+  if(check.checked){
+    seleccion.classList.add('votado');
+  }else {
+    seleccion.classList.remove('votado');
+  };
 }
